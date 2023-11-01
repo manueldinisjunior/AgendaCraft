7d139190dac117f25e34142172b7d1c285be87ab
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-11-01 16:44:24 +0200

    Agenda Craft

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
         <Calendar />
         <TaskForm/>
       </CalendarState>
-      <a href="https://manueldinisjunior.github.io/AgendaCraft/" target="_blank"><i className="fa">&#xf09b;</i> Source Code </a>
     </div>
   );
 }
